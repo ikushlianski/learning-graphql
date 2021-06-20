04f4d272e11e9c9ceda0569db759ffa88cbca1be
Ilya Kushlianski <kushliansky@gmail.com>
2021-06-20 20:41:35 +0300

    add resolvers for updating and removing hackernews links

--- hackernews/hackernews-node/src/index.js
+++ hackernews/hackernews-node/src/index.js
@@ -1,44 +1,60 @@
-const { ApolloServer } = require('apollo-server');
+const path = require("path");
+const fs = require("fs");
+const { ApolloServer } = require("apollo-server");
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
+const links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
-
-const links = [{
-  id: "link-0",
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}];
-
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => null,
     feed: () => links,
+    link: (parent, { id }) => links.find((link) => link.id === id),
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation: {
+    createPost: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+
+      return link;
+    },
+    updateLink: (parent, { id, url, description }) => {
+      const linkToUpdate = links.find((link) => link.id === id);
+
+      if (linkToUpdate) {
+        linkToUpdate.url = url ?? linkToUpdate.url;
+        linkToUpdate.description = description ?? linkToUpdate.description;
+      }
+
+      return linkToUpdate;
+    },
+    removeLink: (parent, { id }) => {
+      const byLinkId = (link) => link.id === id;
+      const linkToDelete = links.find(byLinkId);
+      const linkToDeleteIndex = links.findIndex(byLinkId);
+
+      if (linkToDelete) {
+        links.splice(linkToDeleteIndex, 1);
+      }
+    },
+  },
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+});
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
