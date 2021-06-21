ce8300f00a5803c101abf9bac6a31a6df378cdba
Ilya Kushlianski <kushliansky@gmail.com>
2021-06-21 18:09:34 +0300

    Get links and single link from real DB

--- hackernews/hackernews-node/src/index.js
+++ hackernews/hackernews-node/src/index.js
@@ -1,60 +1,58 @@
 const path = require("path");
 const fs = require("fs");
 const { ApolloServer } = require("apollo-server");
+const { PrismaClient } = require("@prisma/client");
 
-const links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL",
-  },
-];
-
-let idCount = links.length;
+const prisma = new PrismaClient();
 
 const resolvers = {
   Query: {
     info: () => null,
-    feed: () => links,
-    link: (parent, { id }) => links.find((link) => link.id === id),
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
+    link: (parent, { id }, context) => {
+      return context.prisma.link.findUnique({ where: { id: Number(id) } });
+    },
   },
   Mutation: {
-    createPost: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-
-      return link;
+    createPost: (parent, args, context, info) => {
+      return context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
     },
-    updateLink: (parent, { id, url, description }) => {
-      const linkToUpdate = links.find((link) => link.id === id);
-
-      if (linkToUpdate) {
-        linkToUpdate.url = url ?? linkToUpdate.url;
-        linkToUpdate.description = description ?? linkToUpdate.description;
-      }
-
-      return linkToUpdate;
-    },
-    removeLink: (parent, { id }) => {
-      const byLinkId = (link) => link.id === id;
-      const linkToDelete = links.find(byLinkId);
-      const linkToDeleteIndex = links.findIndex(byLinkId);
-
-      if (linkToDelete) {
-        links.splice(linkToDeleteIndex, 1);
-      }
-    },
+    // todo were implemented with an in-memory array, same principle applies with Prisma
+    // updateLink: (parent, { id, url, description }) => {
+    //   const linkToUpdate = links.find((link) => link.id === id);
+    //
+    //   if (linkToUpdate) {
+    //     linkToUpdate.url = url ?? linkToUpdate.url;
+    //     linkToUpdate.description = description ?? linkToUpdate.description;
+    //   }
+    //
+    //   return linkToUpdate;
+    // },
+    // removeLink: (parent, { id }) => {
+    //   const byLinkId = (link) => link.id === id;
+    //   const linkToDelete = links.find(byLinkId);
+    //   const linkToDeleteIndex = links.findIndex(byLinkId);
+    //
+    //   if (linkToDelete) {
+    //     links.splice(linkToDeleteIndex, 1);
+    //   }
+    // },
   },
 };
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
