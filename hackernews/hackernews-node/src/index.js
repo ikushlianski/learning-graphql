846e9e1ef4c6be74dbc0e4724384af75aa11b812
Ilya Kushlianski <kushliansky@gmail.com>
2021-06-25 18:09:20 +0300

    refactor resolvers, add user entity

--- hackernews/hackernews-node/src/index.js
+++ hackernews/hackernews-node/src/index.js
@@ -1,57 +1,32 @@
 const path = require("path");
 const fs = require("fs");
+const { getUserId } = require("./utils.js");
 const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 
 const resolvers = {
-  Query: {
-    info: () => null,
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany();
-    },
-    link: (parent, { id }, context) => {
-      return context.prisma.link.findUnique({ where: { id: Number(id) } });
-    },
-  },
-  Mutation: {
-    createPost: (parent, args, context, info) => {
-      return context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      });
-    },
-    // todo were implemented with an in-memory array, same principle applies with Prisma
-    // updateLink: (parent, { id, url, description }) => {
-    //   const linkToUpdate = links.find((link) => link.id === id);
-    //
-    //   if (linkToUpdate) {
-    //     linkToUpdate.url = url ?? linkToUpdate.url;
-    //     linkToUpdate.description = description ?? linkToUpdate.description;
-    //   }
-    //
-    //   return linkToUpdate;
-    // },
-    // removeLink: (parent, { id }) => {
-    //   const byLinkId = (link) => link.id === id;
-    //   const linkToDelete = links.find(byLinkId);
-    //   const linkToDeleteIndex = links.findIndex(byLinkId);
-    //
-    //   if (linkToDelete) {
-    //     links.splice(linkToDeleteIndex, 1);
-    //   }
-    // },
-  },
+  Query,
+  Mutation,
+  User,
+  Link,
 };
+
+const prisma = new PrismaClient();
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
-  context: {
-    prisma,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req?.headers?.authorization ? getUserId(req) : null,
+    };
   },
 });
 
